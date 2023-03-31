3b806d814f26c231b0ca980c103087a34349a2b9
Howard Yen <hwrdpvt@gmail.com>
2023-03-30 22:10:14 -0400

    Updated the latest information

--- script.js
+++ script.js
@@ -125,6 +125,7 @@ window.addEventListener('scroll', scrollHeader)
 /*==================== SHOW SCROLL UP ====================*/ 
 function scrollUp(){
     const scrollUp = document.getElementById('scroll-up');
+	scrollUp.setAttribute("href", "#home");
     // When the scroll is higher than 560 viewport height, add the show-scroll class to the a tag with the scroll-top class
     if(this.scrollY >= 560) scrollUp.classList.add('show-scroll'); else scrollUp.classList.remove('show-scroll')
 }
